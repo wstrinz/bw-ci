f0e224b3a605a76fa0e54d6722b6e0fccd4a572a
Will Strinz <wstrinz@gmail.com>
2014-05-29 11:52:51 -0500

    use old script tag style requires for now

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,23 +1,20 @@
-var libs = [
-              'javascripts/lib/underscore/underscore.js',
-              'javascripts/lib/jquery/dist/jquery.js',
-              'javascripts/lib/json2/json2.js',
-              'javascripts/lib/backbone/backbone.js',
-              //'javascripts/lib/backbone.marionette.js',
-              //'javascripts/lib/backbone.wreqr.js',
-              //'javascripts/lib/backbone.babysitter.js',
-              'javascripts/models/job.js',
-              'javascripts/models/repository.js',
-              'javascripts/models/repositories.js',
-              'javascripts/views/repository_view.js',
-              'javascripts/views/repositories_view.js',
-              'javascripts/router.js',
-           ]
-
-require(libs, function (foo) {
-  console.log('requires done')
-  $(function(){ CIApp.start() })
-});
-
-
-
+// var libs = [
+//               'javascripts/lib/underscore/underscore.js',
+//               'javascripts/lib/jquery/dist/jquery.js',
+//               'javascripts/lib/json2/json2.js',
+//               'javascripts/lib/backbone/backbone.js',
+//               //'javascripts/lib/backbone.marionette.js',
+//               //'javascripts/lib/backbone.wreqr.js',
+//               //'javascripts/lib/backbone.babysitter.js',
+//               'javascripts/models/job.js',
+//               'javascripts/models/repository.js',
+//               'javascripts/models/repositories.js',
+//               'javascripts/views/repository_view.js',
+//               'javascripts/views/repositories_view.js',
+//               'javascripts/router.js',
+//           ]
+//
+// require(libs, function (foo) {
+//   console.log('requires done')
+// });
+$(function(){ CIApp.start() })
