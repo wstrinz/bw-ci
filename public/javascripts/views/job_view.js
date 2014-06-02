01758e4132bbd960237a3ad880efca20b2f0bf00
Will Strinz <wstrinz@gmail.com>
2014-06-02 13:56:00 -0500

    start building job edit form

--- public/javascripts/views/job_view.js
+++ public/javascripts/views/job_view.js
@@ -1,9 +1,23 @@
 JobView = Backbone.Marionette.ItemView.extend({
   template: Handlebars.compile('<h1>Job</h1>' +
-                               '<h3>Job Name</h3>' +
-                               '<p>{{job_name}}</p>' +
-                               '<h3>Github Repo</h3>' +
-                               '<p>{{github_repo}}</p>' +
-                               '<h3>Build Script</h3>' +
-                               '<p>{{build_script}}</p>' )
+                               '<form name="input" action="job/{{job_name}}">' +
+                                  '<div>' +
+                                    '<label for="job_name">Job Name</h3>' +
+                                  '</div>' +
+                                  '<div>' +
+                                    '<input type="text" id="job_name" name="job_name" value="{{job_name}}"/>' +
+                                  '</div>' +
+                                  '<div>' +
+                                    '<label for="github_repo">Github Repo</h3>' +
+                                  '</div>' +
+                                  '<div>' +
+                                    '<input type="text" disabled="disabled" id="github_repo" name="github_repo" value="{{github_repo}}"/>' +
+                                  '</div>' +
+                                  '<div>' +
+                                    '<label for="build_script">Build Script</h3>' +
+                                  '</div>' +
+                                  '<div>' +
+                                    '<textarea rows=8 cols=60 id="build_script" name="build_script">{{build_script}}</textarea>' +
+                                  '</div>' +
+                               '</form>')
 })
